5a29bb0789a73c476f7aa56bb875f47569079b16
French Countryside Living <reini@frenchcountrysideliving.com>
2026-04-13 20:06:41 +0200

    Revert "Use Marcel as the nav bar logo instead of the fish icon"
    This reverts commit e2fc4badfd1a9bcd60944458d0294fd2d6f22d5f.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -39,8 +39,8 @@ export default function Navigation() {
             className="flex items-center gap-4 shrink-0 group"
           >
             <Image
-              src="/images/marcel-default.png"
-              alt="Marcel"
+              src="/icons/Logo1.png"
+              alt="French Countryside Living"
               width={80}
               height={80}
               className="rounded-full"
